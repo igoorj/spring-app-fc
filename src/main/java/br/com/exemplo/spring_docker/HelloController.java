6e60485223f34b6efc4d0bbdd0c4355afb2b2e04
Igor Goncalves <goncalvesigor67@gmail.com>
2024-06-22 09:55:25 -0300

    teste

--- src/main/java/br/com/exemplo/spring_docker/HelloController.java
+++ src/main/java/br/com/exemplo/spring_docker/HelloController.java
@@ -1,5 +1,6 @@
 package br.com.exemplo.spring_docker;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
+    @Value("${config}")
+    private String variavel;
 
     @RequestMapping(method = RequestMethod.GET)
     public String hello() {
