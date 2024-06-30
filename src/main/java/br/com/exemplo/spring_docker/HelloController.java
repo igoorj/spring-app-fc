c33b13f015432122659d69791f6d7138dbdaabde
Igor Goncalves <goncalvesigor67@gmail.com>
2024-06-30 20:02:53 -0300

    testando code smell

--- src/main/java/br/com/exemplo/spring_docker/HelloController.java
+++ src/main/java/br/com/exemplo/spring_docker/HelloController.java
@@ -3,11 +3,17 @@ package br.com.exemplo.spring_docker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.atomic.AtomicLong;
+
 @RestController
 public class HelloController {
 
     @GetMapping
-    public String hello() {
-        return "<h1>Ola mundo, ola docker!!!</h1>";
+    public Boolean hello() {
+        return isSameNumberValue(new AtomicLong(10L), new AtomicLong(11L));
+    }
+
+    Boolean isSameNumberValue(AtomicLong a, AtomicLong b) {
+        return a.equals(b);
     }
 }
